6c0b96b29f93abf5d8b3ba79af4a9a7f8f7151fc
helya moradi <helya.m78@gmail.com>
2022-10-26 22:08:13 +0330

    fix responsive bug and add localstorage to project

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let autoCompleteBox = $.querySelector('.auto-complete');
 searchInput.addEventListener('focus', () => {
     wrappContainer.classList.add('focus');
 })
+
 searchInput.addEventListener('blur', () => {
     wrappContainer.classList.remove('focus');
     // wrappContainer.classList.remove('active');
@@ -17,11 +18,15 @@ searchInput.addEventListener('keyup', (e) => {
     if (searchInputValue) {
         wrappContainer.classList.add('active');
 
+        let newSuggestion = JSON.parse(localStorage.getItem('suggestion'));
+        suggestion = newSuggestion;
         let suggestArray = suggestion.filter((word) => {
             return word.toLowerCase().startsWith(searchInputValue.toLocaleLowerCase());
         })
         suggestGenerator(suggestArray);
         if (e.code === 'Enter') {
+            suggestion.push(searchInputValue);
+            localStorage.setItem('suggestion', JSON.stringify(suggestion));
             location.href = `https://www.google.com/search?q=${searchInputValue}`;
             searchInput.value = '';
             wrappContainer.classList.remove('active');
@@ -34,13 +39,13 @@ searchInput.addEventListener('keyup', (e) => {
 
 function suggestGenerator(suggestArray) {
     let autoListItems = suggestArray.map((word) => {
-        return '<li><i class="fas fa-search"></i>' + word + '</li>';
+        return '<li><i class="fas fa-search"></i>' + word + '<span class="del">Delete</span></li>';
     })
     let customAuto;
     if (autoListItems.length) {
         customAuto = autoListItems.join('');
     } else {
-        customAuto = '<li><i class="fas fa-search"></i>' + searchInput.value + '</li>';
+        customAuto = '<li><i class="fas fa-search"></i>' + searchInput.value + '<span class="del">Delete</span></li>';
     }
     autoCompleteBox.innerHTML = customAuto;
     Select();
@@ -50,12 +55,41 @@ function Select() {
     let listitems = $.querySelectorAll('li');
     listitems.forEach((item) => {
         item.addEventListener('click', (event) => {
-            let inputValue = event.target.textContent;
-            suggestion.push(inputValue);
-            searchInput.value = inputValue;
-            wrappContainer.classList.remove('active');
-            location.href = `https://www.google.com/search?q=${searchInput.value}`;
-            searchInput.value = '';
+            wrappContainer.classList.add('active');
+
+            if (event.target.classList.contains('del')) {
+                searchInput.focus();
+                searchInput.addEventListener('focus', () => {
+                    wrappContainer.classList.add('active');
+                })
+                event.target.parentElement.remove();
+                let inputValue = event.target.parentElement.childNodes[1].data;
+                let newSuggestion = JSON.parse(localStorage.getItem('suggestion'));
+                let newsuggestion = newSuggestion.filter(suggest => {
+                    if (suggest != inputValue) {
+                        return suggest;
+                    }
+                })
+
+                suggestion = newsuggestion;
+                localStorage.setItem('suggestion', JSON.stringify(suggestion));
+
+
+            } else {
+                let inputValue = event.target.childNodes[1].data;
+                suggestion.push(inputValue);
+                searchInput.value = inputValue;
+                wrappContainer.classList.remove('active');
+                location.href = `https://www.google.com/search?q=${searchInput.value}`;
+                searchInput.value = '';
+            }
+
         })
     })
 }
+
+document.body.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('del')) {
+        wrappContainer.classList.remove('active');
+    }
+})
